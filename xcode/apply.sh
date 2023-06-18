5ddcde431c06fbbf00ae577951bf05ad71949b89
TrpFrog <dev@trpfrog.net>
2023-06-18 18:06:19 +0900

    Add xcode settings

---
+++ xcode/apply.sh
@@ -0,0 +1,7 @@
+# Apply Xcode settings
+
+## Change default template
+ln -fs ~/dotfiles/xcode/IDETemplateMacros.plist ~/Library/Developer/Xcode/UserData/
+
+## Add theme using SFMono Square
+ln -fs ~/dotfiles/xcode/'Default (Dark, JA).xccolortheme' ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
